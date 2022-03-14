44750a9e149248e67d320f7185c7f12661242100
Maxim <maxim13danilov@gmail.com>
2022-03-14 14:30:18 +0500

    Add product.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
+const product = require("./backend/api/product")
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const port = process.env.port || 5000
+
+app.use("/backend/api/product", product)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
